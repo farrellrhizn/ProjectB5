bbd0f06ee96b47b355c00557a32fcf5759c51a1a
e41212020_Faris Hasan <100421748+faris2000111@users.noreply.github.com>
2022-11-17 19:04:59 +0700

    Add files via upload

---
+++ laporan.php
@@ -0,0 +1,73 @@
+<?php
+	session_start();
+	$timeout = 1; // setting timeout dalam menit
+	$logout = "login.php"; // redirect halaman logout
+
+	$timeout = $timeout * 360; // menit ke detik
+	if(isset($_SESSION['start_session'])){
+		$elapsed_time = time()-$_SESSION['start_session'];
+		if($elapsed_time >= $timeout){
+			session_destroy();
+			echo "<script type='text/javascript'>alert('Sesi telah berakhir');window.location='$logout'</script>";
+		}
+	}
+
+	$_SESSION['start_session']=time();
+
+	include('config.php');
+	if($_SESSION['status_login'] != true){
+		echo '<script>window.location="login.php"</script>';
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <title>Laporan || Sembakouu</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+	
+    <!-- header -->
+	<header>
+		<div class="container">
+		<h1><a href="home.php">Sembakouu</a></h1>
+			<ul>
+				<li><a href="home.php">Dashboard</a></li>
+				<li><a href="admin.php">Profile</a></li>
+				<li><a href="listbarang.php">Barang</a></li>
+				<li><a href="listsupplier.php">Supplier</a></li>
+				<li><a href="transaksi.php">Transaksi</a></li>
+				<li><a href="laporan.php">Laporan</a></li>
+				<li><a href="logout.php" onClick="return confirm('apakah kamu yakin?')">Logout</a></li>
+			</ul>
+		</div>
+	</header>
+	
+	<!-- Content -->
+	<div class="section">
+		<div class="container">
+			<h2>Laporan</h2>
+			<div class="box">
+				<form action="laporanperhari.php" method="POST">
+					<input type="submit" name="hari" value="Laporan Perhari" class="laporan">
+				</form>
+				<form action="laporanperbulan.php" method="POST">
+					<input type="submit" name="bulan" value="Laporan Perbulan" class="laporan">
+				</form>	
+			</div>
+		</div>
+		<!-- Footer -->
+	<footer class="container">
+		<div class="pull-right">
+			<a href="" target="_blank">Group B5</a>
+		</div>
+		<div class="pull-left">
+			<span>Copyright &copy; 2022 - Sembakouu.</span> © <a href="https://www.instagram.com/farishasan_13/" target="_blank">Developer</a>
+		</div>
+	</footer>
+	</div>
+</body>
+</html>
